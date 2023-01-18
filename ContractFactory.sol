d9a97c2f396a3639b49d03a2bfcae8ca0a9755b1
Mr. SMH <77523793+SMojtabaHosseini@users.noreply.github.com>
2023-01-18 22:06:40 +0330

    Update ContractFactory.sol

--- ContractFactory.sol
+++ ContractFactory.sol
@@ -11,7 +11,8 @@ contract ContractFactory {
     constructor(){
         owner = msg.sender;
     }
-
+    
+    //after receiving tokens, create a new contract and set the msg.sender as deployer
     receive() external payable {
         new NewContract(msg.sender);
         contractCreated += 1;
@@ -23,12 +24,14 @@ contract ContractFactory {
         _;
     }
 
+    //withdraw function for owner of the contract
     function withdraw() payable public onlyOwner {
         uint256 contractBalance = address(this).balance;
         (bool success,) = msg.sender.call{value:contractBalance}("");
         require(success, "withdraw failed!");
     }
 
+    //transfer function to send arbitrary amount of tokens to an arbitrary address directly only by owner of the contract
     function transfer(address payable _to, uint256 _amount) public payable onlyOwner{
         (bool sent, ) = _to.call{value:_amount}("");
         require(sent, "transaction failed!");
